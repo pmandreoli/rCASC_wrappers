026d4fceedff2fcfde2588d1cb69c7beed77e303
pitta <pietro.mandreoli@gmail.com>
2019-03-20 11:18:07 +0100

    first_commit

---
+++ MatrixDimension/DIM.R
@@ -0,0 +1,17 @@
+ 
+# etwd("./home")
+ 
+ library("argparser")
+p <- arg_parser("permutation")
+p <- add_argument(p, "matrixName", help="matrix count name")
+p <- add_argument(p, "format", help="matrix format like csv, txt...")
+p <- add_argument(p, "separator", help="matrix separator ")
+argv <- parse_args(p)
+
+
+matrixName=argv$matrixName
+format=argv$format
+separator=argv$separator
+if(separator=="tab"){separator2="\t"}else{separator2=separator} #BUG CORRECTION TAB PROBLEM 
+#setwd("./../scratch")
+write.table(dim(read.table(paste(matrixName,".",format,sep=""),header=TRUE,row.names=1,sep=separator2)),"dimensions.txt")
