7afe71de8674c0263e16b30eec23494ebc032713
pmandreoli <pietro.mandreoli@gmail.com>
2020-01-21 14:19:44 +0000

    color_update

--- PermutationClustering/files.sh
+++ PermutationClustering/files.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-echo "<h1>Output Permutational Clustering</h1>" > $2
-for filepath in `find "$1" -maxdepth 4 -mindepth 1`;
+echo "<h1 style=\"background-color:MediumSeaGreen;\"> Output Permutational Clustering</h1>" > $2
+for filepath in `find "$1" -maxdepth 3 -mindepth 1`;
 do
 html_path=`echo "$filepath" | sed 's/.*_files\///'`
 path=`basename "$filepath"` 
-
+if [[ ${path} != *"."* ]] ; then
+path=$path" type: DIRECTORY" 
+echo "<br><hr>" >> $2
+fi
 
 echo "<a href="$html_path">$path</a><br>" >> $2
 
