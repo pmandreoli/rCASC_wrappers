f0401ee63a8efce77dbdea1c0c9b1aa763b8c054
Pmandreoli <pietro.mandreoli@gmail.com>
2020-05-20 13:46:42 +0000

    new_wrapper_PCSHARP

---
+++ PermutationalClusteringSHARP/files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##bash wrapper for html output
+##set title
+echo "<h1 style=\"background-color:MediumSeaGreen;\"> Output Permutation clustering SHARP</h1>" > $2
+for filepath in `find "$1" -maxdepth 3 -mindepth 1`;
+do
+html_path=`echo "$filepath" | sed 's/.*_files\///'`
+path=`basename "$filepath"` 
+if [[ ${path} != *"."* ]] ; then
+path=$path" type: DIRECTORY" 
+echo "<br><hr>" >> $2
+fi
+
+echo "<a href="$html_path">$path</a><br>" >> $2
+
+done
